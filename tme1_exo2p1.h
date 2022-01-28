dc3196845737e656755638ce8649623d08d46886
Danaël <98533285+CyranHOEdeBergerac@users.noreply.github.com>
2022-01-28 11:06:48 +0100

    Header avec fct pour tableaux
    J'ai fait un header pour avoir accès aux fonctions sur les tableaux simples dans la partie 2 (eheh big brain time)

---
+++ tme1_exo2p1.h
@@ -0,0 +1,16 @@
+#ifndef TME1_EXO2_H
+#define TME1_EXO2_H
+
+int alloue_tableau(int** T, int n);
+
+int desalloue_tableau(int*t);
+
+void remplir_tableau(int* tab, int taille, int V);
+
+void afficher_tableau(int* tab, int taille);
+
+int somme_v1(int* tab, int taille);
+
+int somme_v2(int*tab, int taille);
+
+#endif
