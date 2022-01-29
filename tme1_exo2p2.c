a1f0ad05e7fdd91341e73d55a984688aa1117aa4
EleaTriesToCode <98588172+EleaTriesToCode@users.noreply.github.com>
2022-01-29 12:46:24 +0100

    Update tme1_exo2p2.c
    J'ai rajouté le main pour gnuploter.
    Y'a deux options pour la valeur de V, je crois que c'est important et que ça explique nos courbes, mais je n'ai pas encore bien compris en quoi!

--- tme1_exo2p2.c
+++ tme1_exo2p2.c
@@ -83,4 +83,41 @@ int mat_diff_v2(int** mat, int taille,int V){
     return 1;
 }
 
+/*Main permettant comparaison par gnuplot*/
+int main(){
+    //int V = 1000; //choix 1 : fixer V pour toutes les itérations
+    FILE* fichier = fopen("sortie_vitesse.txt","w");
+    for (int n=10; n < 10000; n = n+100){
+        /*Création de la matrice*/
+        int taille = n;
+        int V = n*10; //choix 2 : faire varier V selon la taille de la matrice (amène au pire des cas)
+        int** mat;
+        alloue_matrice(&mat,taille);
+        remplir_matrice(mat,taille,V);
+
+        /*Test et comparaison de nos algorithmes*/
+        clock_t temps_initial;
+        clock_t temps_final;
+        double temps_algo1;
+        double temps_algo2;
+
+        temps_initial = clock();
+        mat_diff_v1(mat,taille);
+        temps_final = clock();
+        temps_algo1 = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
+
+        temps_initial = clock();
+        mat_diff_v2(mat,taille,V);
+        temps_final = clock();
+        temps_algo2 = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
+
+        fprintf(fichier,"%d\t",n);
+        fprintf(fichier,"%f\t",temps_algo1);
+        fprintf(fichier,"%f\n",temps_algo2);
+        
+        /*Libération de la mémoire*/
+        desalloue_matrice(mat,taille);
+    }
+}
+
 
