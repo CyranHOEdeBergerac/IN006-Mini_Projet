df6b35ce0dd73455f13f04a61e96fdb2e9168b25
EleaTriesToCode <98588172+EleaTriesToCode@users.noreply.github.com>
2022-01-30 12:56:06 +0100

    Update tme1_exo2p1.c
    Rajout du coup des fonctions sommes qui vont avec le main

--- tme1_exo2p1.c
+++ tme1_exo2p1.c
@@ -6,6 +6,34 @@
 #include<assert.h>
 #include "fonctions_tableau.h"
 
+/*2.2*/
+
+int somme_v1(int* tab,int taille){
+	int res = 0;
+	int val;
+	for (int i = 0; i<taille; i++){
+		for (int j =0; j<taille; j++){
+			val = (tab[i] - tab[j]);
+			res += val*val;
+		}
+	}
+	return res;
+}
+/*La complexité est bien en O(n**2) : la première boucle fait n opérations,
+et la seconde boucle imbriquée en fait n aussi. Ainsi à chaque itération de
+la première on effectue n opérations.*/
+
+int somme_v2(int* tab, int taille){
+	int somme = 0;
+    int somme_carre = 0;
+    for(int i = 0 ; i < taille ; i++){
+        somme += tab[i];               //On effectue les deux sommes de notre formule
+        somme_carre += tab[i]*tab[i];
+    }
+    return 2*taille*somme_carre - 2* somme*somme;   //On en fait la somme avec les constantes calculées
+}
+
+
 /*Fonction main permettant d'obtenir les graphiques gnuplot représentant les temps de calcul de nos deux algorithmes*/
 int main(){
    /*Ouverture du fichier qui stockera nos valeurs de temps de calcul*/
