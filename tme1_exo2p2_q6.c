540fb7e6dfb2264f0415802284c22d065c9cfc88
Danaël <98533285+CyranHOEdeBergerac@users.noreply.github.com>
2022-01-30 11:17:30 +0100

    2.6 algo moins complexe
    Bon du coup d'abord j'ai changé deux petites coquilles (int** pour la déclaration et produit des coefficients à la place de la somme), ensuite je pense avoir trouvé une solution un peu moins complexe pour le produit je vois pas comment on peut faire mieux quoi, juste on évite de calculer les 0*qch dans notre somme de produits quoi...
    
    Tu me diras ce que t'en penses, je vais calculer la complexité et compléter le rapport

--- tme1_exo2p2_q6.c
+++ tme1_exo2p2_q6.c
@@ -1,16 +1,38 @@
 /*On considère que des matrices carrées pour réutiliser nos fonctions*/
 
-/*Calcul du produit de deux matrices en O(n^3)*/
-tab** pdt_mat_v1(int** mat1, int** mat2, int taille){
+/*Fonction calculant le produit de deux matrices carrées en O(n^3)*/
+int** pdt_mat_v1(int** mat1, int** mat2, int taille){
 	int** res;
 	alloue_matrice(&res,taille);
 	for (int ligne = 0; ligne < taille; ligne++){
 		for (int col = 0; col < taille; col++){
 			int coef;
 			for (int k = 0; k < taille; k++){
-				coef += (mat1[ligne][k] + mat2[k][col]);
+				coef += (mat1[ligne][k] * mat2[k][col]);
 			}
 			res[ligne][col] = coef;
+		}
+	}
+	return res;
+}
+
+
+/*Fonction calculant le produit de deux matrices carrées avec une complexité un peu moins importante prenant en compte le fait que 
+Les deux matrices sont triangulaires*/
+
+int** pdt_mat_v2(int** mat1, int** mat2, int taille){
+	int** res; 
+	alloue_matrice(&res, taille);
+	int coeff; 		//Sera utilisé pour chaque élément de notre matrice finale en stockant les sommes des produits successifs
+	
+	
+	for(int i = 0; i < taille ; i++){
+		for(int j = i ; j < taille ; j++){
+			coeff = 0;
+			for(int k = j ; k < taille ; k ++){
+				coeff += mat1[i][k]*mat2[k][j];
+			}
+			res[i][j] = coeff;
 		}
 	}
 	return res;
