992784e027fac52cca8167260c76b1a366db9fd2
EleaTriesToCode <98588172+EleaTriesToCode@users.noreply.github.com>
2022-01-30 13:26:13 +0100

    Update tme1_exo2p2_q6.c
    rajout du main pour gnuplotter

--- tme1_exo2p2_q6.c
+++ tme1_exo2p2_q6.c
@@ -42,3 +42,50 @@ int** pdt_mat_v2(int** mat1, int** mat2, int taille){
 	}
 	return res;
 }
+
+/*Main permettant comparaison par gnuplot*/
+int main(){
+    int V = 200;
+    FILE* fichier = fopen("sortie_vitesse.txt","w");
+    for (int n=10; n < 1000; n = n+100){
+        /*Création de la matrice*/
+        int taille = n;
+        int** mat1;
+        int** mat2;
+        alloue_matrice(&mat1,taille);
+        alloue_matrice(&mat2,taille);
+        remplir_matrice(mat1,taille,V);
+        remplir_matrice(mat2,taille,V); /*L'objectif ici n'est pas tant d'avoir un résultat mathématique correct 
+		que de comparer la complexité temporelle des algorithmes. Le pdt_mat_v2 traitera le même nombre de coefficients
+		que les matrices données en argument soient réellement ou non triangulaires. Le résultat sera erroné, mais c'est 
+		ici sans importance.*/
+
+        /*Test et comparaison de nos algorithmes*/
+        clock_t temps_initial;
+        clock_t temps_final;
+        double temps_algo1;
+        double temps_algo2;
+        int** res;
+
+        temps_initial = clock();
+        res = pdt_mat_v1(mat1,mat2,taille);
+        temps_final = clock();
+        temps_algo1 = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
+
+        temps_initial = clock();
+        res = pdt_mat_v2(mat1,mat2,taille);
+        temps_final = clock();
+        temps_algo2 = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
+
+        fprintf(fichier,"%d\t",n);
+        fprintf(fichier,"%f\t",temps_algo1);
+        fprintf(fichier,"%f\n",temps_algo2);
+        
+        /*Libération de la mémoire*/
+        desalloue_matrice(res,taille);
+        desalloue_matrice(mat1,taille);
+        desalloue_matrice(mat2,taille);
+    }
+    /*Fermeture du fichier*/
+    fclose(fichier);
+}
