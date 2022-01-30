4e147ea008506ea78e6ed4554dfda8d18eb95101
EleaTriesToCode <98588172+EleaTriesToCode@users.noreply.github.com>
2022-01-30 12:55:28 +0100

    Update fonctions_tableau.c
    Je choisi de déplacer les fonctions de la 2.2 dans tme1_exo2p1 car on ne les réutilise pas par la suite (elles vont avec ce main précis)

--- fonctions_tableau.c
+++ fonctions_tableau.c
@@ -30,30 +30,3 @@ void afficher_tableau(int* tab, int taille){
 		printf("%d \t",tab[i]);
 	}printf(" ]\n");
 }
-
-/*2.2*/
-
-int somme_v1(int* tab,int taille){
-	int res = 0;
-	int val;
-	for (int i = 0; i<taille; i++){
-		for (int j =0; j<taille; j++){
-			val = (tab[i] - tab[j]);
-			res += val*val;
-		}
-	}
-	return res;
-}
-/*La complexité est bien en O(n**2) : la première boucle fait n opérations,
-et la seconde boucle imbriquée en fait n aussi. Ainsi à chaque itération de
-la première on effectue n opérations.*/
-
-int somme_v2(int* tab, int taille){
-	int somme = 0;
-    int somme_carre = 0;
-    for(int i = 0 ; i < taille ; i++){
-        somme += tab[i];               //On effectue les deux sommes de notre formule
-        somme_carre += tab[i]*tab[i];
-    }
-    return 2*taille*somme_carre - 2* somme*somme;   //On en fait la somme avec les constantes calculées
-}
